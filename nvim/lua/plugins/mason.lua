cc8627444d29605a950dbfc605a834d52d3939f7
Torbiee <jack@phthalo.net>
2026-04-20 21:22:26 -0400

    Initial Commit

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return{
+	"mason-org/mason.nvim",
+	opts={
+	   ui = {
+	       icons ={
+		   packages_installed = "./",
+		   packages_pending = "->",
+		   packages_uninstalled = "X"
+	       }
+	   }
+	}
+}
---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,4 @@
+return {
+	{"nvim-tree/nvim-tree.lua", opts={}},
+	{"nvim-tree/nvim-web-devicons", opts={}}
+}
